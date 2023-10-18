af4b08c0b34d26427c38ad3c1b615144c1814c26
nsuazo <nicholas.suazo@gmail.com>
2023-10-18 11:04:23 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,21 +10,6 @@ resource "aws_vpc" "example_vpc" {
   }
 }
 
-# Create a security group
-resource "aws_security_group" "example_sg" {
-  name        = var.security_group_name
-  description = "Example security group"
-  vpc_id      = aws_vpc.example_vpc.id
-
-  # Define inbound rule for SSH
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = var.ssh_cidr_blocks
-  }
-}
-
 # Create a subnet within the VPC
 resource "aws_subnet" "example_subnet" {
   vpc_id                  = aws_vpc.example_vpc.id
@@ -36,14 +21,14 @@ resource "aws_subnet" "example_subnet" {
   }
 }
 
-# Create an EC2 instance
+# Create EC2 instances
 resource "aws_instance" "example" {
+  count         = var.num_instances
   ami           = var.ami_id
   instance_type = var.instance_type
   key_name      = var.key_pair_name
-  vpc_security_group_ids = [aws_security_group.example_sg.id]
   subnet_id     = aws_subnet.example_subnet.id
   tags = {
-    Name = var.ec2_instance_name
+    Name = "${var.ec2_instance_name}-${count.index}"
   }
 }
