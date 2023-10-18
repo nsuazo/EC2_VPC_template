3dd83d3e8d5229b12763ce50b477dd8ccbb93976
nsuazo <nicholas.suazo@gmail.com>
2023-10-18 09:39:52 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,15 +5,18 @@ provider "aws" {
 # Create a VPC
 resource "aws_vpc" "example_vpc" {
   cidr_block = var.vpc_cidr
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
 # Create a security group
 resource "aws_security_group" "example_sg" {
-  name        = "example-sg"
+  name        = var.security_group_name
   description = "Example security group"
   vpc_id      = aws_vpc.example_vpc.id
 
-  # Define inbound rules as needed
+  # Define inbound rule for SSH
   ingress {
     from_port   = 22
     to_port     = 22
@@ -28,9 +31,8 @@ resource "aws_subnet" "example_subnet" {
   cidr_block              = var.subnet_cidr
   availability_zone       = var.availability_zone
   map_public_ip_on_launch = true
-
   tags = {
-    Name = "example-subnet"
+    Name = var.subnet_name
   }
 }
 
@@ -38,7 +40,10 @@ resource "aws_subnet" "example_subnet" {
 resource "aws_instance" "example" {
   ami           = var.ami_id
   instance_type = var.instance_type
-  key_name      = var.key_name
+  key_name      = var.key_pair_name
   vpc_security_group_ids = [aws_security_group.example_sg.id]
   subnet_id     = aws_subnet.example_subnet.id
+  tags = {
+    Name = var.ec2_instance_name
+  }
 }
